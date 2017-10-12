fd05fa1c1fd3dd3b412e2f8a741dca8aed0905f8
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-10-12 18:45:01 -0500

    flask web page with home and log in page (not storing info yet)

---
+++ FlaskApp/app/views.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from app import app
+from .forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    #fake user
+    user = {'nickname': 'Jenny'}
+    # fake array of posts
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validation_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+              return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
