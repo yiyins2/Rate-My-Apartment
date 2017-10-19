08a94b129cfa9c799e4c75e0aeccecd412def577
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-10-19 11:59:34 -0500

    added configurations for providers and database path

--- FlaskApp/config.py
+++ FlaskApp/config.py
@@ -1,2 +1,13 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'Meow---meow'
+
+OPENID_PROVIDERS = [
+    {'name' : 'Google', 'url': 'https://myaccount.google.com'},
+    {'name' : 'Yahoo', 'url' : 'http://openid.yahoo.com'}
+]
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
