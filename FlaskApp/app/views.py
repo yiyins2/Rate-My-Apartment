e4bf45b37deff4a900c86239232fca6356d5b898
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-10-19 12:01:39 -0500

    fixed some bugs

--- FlaskApp/app/views.py
+++ FlaskApp/app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
@@ -23,8 +23,11 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
-    if form.validation_on_submit():
+    if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' %
               (form.openid.data, str(form.remember_me.data)))
-              return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+        return redirect('/index')
+    return render_template('login.html',
+                            title='Sign In',
+                            form=form,
+                            providers=app.config['OPENID_PROVIDERS'])
